f705aa6c1c84926d10e974b829e70bf8eb32a256
Bengur2 <pernicaondrej2@gmail.com>
2025-03-08 22:51:05 +0100

    Test4

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ io.on('connection', (socket) => {
 
     socket.on('roll', () => {
         const player = players.find(p => p.id === socket.id);
-        if (player && player.rollsCount < 2) {
-            player.rolls.push(Math.floor(Math.random() * 100) + 1); // Generování čísla od 1 do 100
-            player.rollsCount++;
+        if (player && player.rollsCount === 0) {
+            for (let i = 0; i < 2; i++) {
+                player.rolls.push(Math.floor(Math.random() * 100) + 1);
+            }
+            player.rollsCount = 2;
             io.emit('updateResults', sortPlayers(players));
         }
     });
@@ -56,8 +58,14 @@ server.listen(port, () => {
 
 function sortPlayers(players) {
     return players.slice().sort((a, b) => {
-        const sumA = a.rolls.reduce((acc, val) => acc + val, 0);
-        const sumB = b.rolls.reduce((acc, val) => acc + val, 0);
-        return sumB - sumA;
+        if (b.rolls.length === 0 && a.rolls.length > 0) return 1;
+        if (a.rolls.length === 0 && b.rolls.length > 0) return -1;
+        if (a.rolls.length === 0 && b.rolls.length === 0) return 0;
+
+        if (b.rolls[0] !== a.rolls[0]) {
+            return b.rolls[0] - a.rolls[0]; // Porovnání podle prvního hodu
+        } else {
+            return b.rolls[1] - a.rolls[1]; // Porovnání podle druhého hodu, pokud jsou první hody stejné
+        }
     });
 }
