1bd3f53f31443fe3297a85d7ae8adea2c1b372a9
Bengur2 <pernicaondrej2@gmail.com>
2025-03-08 22:38:24 +0100

    Úprava hodu kostkou

--- server.js
+++ server.js
@@ -6,28 +6,30 @@ const server = http.createServer(app);
 const io = socketIo(server);
 app.use(express.static(__dirname));
 
-const players = []; // Pole pro ukládání hráčů
+const players = [];
 
 io.on('connection', (socket) => {
     socket.on('join', (name) => {
-        players.push({ id: socket.id, name: name, roll: 0 });
-        io.emit('updateResults', players);
+        players.push({ id: socket.id, name: name, rolls: [], rollsCount: 0 });
+        io.emit('updateResults', sortPlayers(players));
     });
 
     socket.on('roll', () => {
         const player = players.find(p => p.id === socket.id);
-        if (player) {
-            player.roll = Math.floor(Math.random() * 6) + 1;
-            io.emit('updateResults', players);
+        if (player && player.rollsCount < 2) {
+            player.rolls.push(Math.floor(Math.random() * 100) + 1); // Generování čísla od 1 do 100
+            player.rollsCount++;
+            io.emit('updateResults', sortPlayers(players));
         }
     });
 
     socket.on('reset', (name) => {
         if (name === "Master") {
             players.forEach(player => {
-                player.roll = 0;
+                player.rolls = [];
+                player.rollsCount = 0;
             });
-            io.emit('updateResults', players);
+            io.emit('updateResults', sortPlayers(players));
         }
     });
 
@@ -35,13 +37,14 @@ io.on('connection', (socket) => {
         const index = players.findIndex(p => p.id === socket.id);
         if (index !== -1) {
             players.splice(index, 1);
-            io.emit('updateResults', players);
+            io.emit('updateResults', sortPlayers(players));
         }
         if (players.length === 0) {
             players.forEach(player => {
-                player.roll = 0;
+                player.rolls = [];
+                player.rollsCount = 0;
             });
-            io.emit('updateResults', players);
+            io.emit('updateResults', sortPlayers(players));
         }
     });
 });
@@ -50,3 +53,9 @@ const port = 3000;
 server.listen(port, () => {
     console.log(`Server běží na portu ${port}`);
 });
+
+function sortPlayers(players) {
+    return players.slice().sort((a, b) => {
+        const sumA = a.rolls.reduce((acc, val) => acc + val, 0);
+        const sumB = b.rolls.reduce((acc, val) => acc + val, 0);
+        return sumB - sumA;
